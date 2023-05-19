7e00e7265787994bd6438460da24f92c7e041d87
Chaitanya Sonavane <techycs7299@gmail.com>
2023-05-19 20:56:45 +0000

    Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.

--- African Cities.sql
+++ African Cities.sql
@@ -1,4 +1,4 @@
-#Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+# Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
 # Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 
 # SOLUTION
