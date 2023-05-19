01bd3cc45a6d0917858dc959b3924ff7e75ce9ed
Chaitanya Sonavane <techycs7299@gmail.com>
2023-05-19 21:13:33 +0000

    Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.

---
+++ Average Population of Each Continent.sql
@@ -0,0 +1,46 @@
+--Problem Statement
+/*
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+Input Format
+
+The CITY and COUNTRY tables are described as follows:
+
+City
+
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | int(11)  |
+| Name        | char(35) |
+| CountryCode | char(3)  |
+| District    | char(20) |
+| Population  | int(11)  |
++-------------+----------+
+Country
+
++----------------+-------------+
+| Field          | Type        |
++----------------+-------------+
+| Code           | char(3)     |
+| Name           | char(52)    |
+| Continent      | char(50)    |
+| Region         | char(26)    |
+| SurfaceArea    | float(10,2) |
+| IndepYear      | smallint(6) |
+| Population     | int(11)     |
+| LifeExpectancy | float(3,1)  |
+| GNP            | float(10,2) |
+| GNPOld         | float(10,2) |
+| LocalName      | char(45)    |
+| GovernmentForm | char(45)    |
+| HeadOfState    | char(60)    |
+| Capital        | int(11)     |
+| Code2          | char(2)     |
++----------------+-------------+
+*/
+--Solution
+
+select co.continent, floor(avg(ci.population)) from country as co, city as ci where ci.countrycode=co.code group by co.continent;
